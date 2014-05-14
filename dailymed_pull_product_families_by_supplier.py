858437c2dd3695ace9d2c9064a3519b5fc202b7d
ETF <ETF@users.noreply.github.com>
2014-05-14 14:40:57 -0700

    Update dailymed_pull_product_families_by_supplier.py
    Revising code to take into account +100 drug families returned by API

--- dailymed_pull_product_families_by_supplier.py
+++ dailymed_pull_product_families_by_supplier.py
@@ -1,8 +1,7 @@
 """
 This script pulls a list of supplier's drug families listed on DailyMed.
 Change supplier object as needed.
-Script can be modified for suppliers with more than 100 products or
-more than one page of returned from API call.
+
 """
 
 import requests
@@ -10,27 +9,49 @@ from pandas.io.json import json_normalize
 from time import strftime
 import pandas as pd
 
-def dailymed_lookup():
+supplier = 'Sandoz'
 
-    """Execute API call, massage and export pandas DataFrame"""
-    supplier = 'Northstar'
+def how_many_pages():
+    """How many pages of information exist?"""
+
     query = 'http://dailymed.nlm.nih.gov/dailymed/services/v2/spls.json?labeler=%s' % supplier
     text = requests.get(query)
     api_response = text.json()
-    blue_data = json_normalize(api_response['data'])
+    global num_elements
+    num_elements = int(api_response['metadata']['total_elements'])
+    number_of_pages, remainder = divmod(num_elements, 100)
 
-    s = blue_data['title']
-    s.name = 'manufacturer'
-    del blue_data['title']
+    if remainder > 0:
+        number_of_pages += 1
 
-    blue_data = blue_data.join(s.apply(lambda x: pd.Series(x.split('['))))
-    blue_data['drug_dose'] = blue_data[0]
-    blue_data['manufacturer_dirty'] = blue_data[1]
-    blue_data['manufacturer'] = blue_data['manufacturer_dirty'][0][:-1]
-    blue_data = blue_data.drop([0, 1, 'manufacturer_dirty'], axis=1)
+    return number_of_pages
+
+def dailymed_lookup(number_of_pages):
+    """Execute API call, massage data, and export pandas DataFrame"""
+
+    blue_list = []
+
+    for page in xrange(1, (number_of_pages + 1)):
+        query = 'http://dailymed.nlm.nih.gov/dailymed/services/v2/spls.json?labeler=%s&page=%s' % (supplier, page)
+        text = requests.get(query)
+        api_response = text.json()
+        blue_list.append(json_normalize(api_response['data']))
+
+    blue_data = pd.concat(blue_list)
+    s = pd.DataFrame(blue_data.title.str.split('[').tolist())
+    s.rename(columns={0: 'DRUG_FAMILY', 1 :'SUPPLIER'}, inplace=True)
+
+    replace_stuff = [']', ',', '.']
+    for item in replace_stuff:
+        s['SUPPLIER'] = s['SUPPLIER'].str.replace(item, '')
+    blue_data = blue_data.drop(['title'], axis=1)
+
     blue_data.columns = [x.upper() for x in blue_data.columns]
     blue_data.to_csv('%s_dailymed_%s_drug_list_pulled.csv' %
                                 (strftime('%Y%m%d'), supplier))
 
-dailymed_lookup()
-                                
+    print '%s elements returned by API call for %s' % (num_elements -1, supplier)
+
+number_of_pages = how_many_pages()
+dailymed_lookup(number_of_pages)
+        
